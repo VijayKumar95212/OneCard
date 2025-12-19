12ffad8e2eb90ac7fc83ec6cc215c9b802527d02
vijay <vijaykumarsimar1>
2025-12-19 13:55:45 +0530

    fix 2

--- OneCard-frontend/react/src/Componentes/admin/GetAllProduct/GetProduct.jsx
+++ OneCard-frontend/react/src/Componentes/admin/GetAllProduct/GetProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./GetProduct.css";
+import "./Getproduct.css";
 import { Link } from "react-router-dom";
 
 const GetProduct = () => {
