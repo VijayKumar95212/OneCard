e97d31353e21ba86e9dc2ad31c66cf2f3029f79a
vijay <vijaykumarsimar1>
2025-12-19 13:58:22 +0530

    fix 3

--- OneCard-frontend/react/src/Pages/LoginSignup.jsx
+++ OneCard-frontend/react/src/Pages/LoginSignup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import {baseUrlFun} from "../Base-url"
-import './Css/LoginSignup.css';
+import './Css/Loginsignup.css';
 import { useNavigate } from 'react-router-dom';
 
 const Base_url = baseUrlFun();
